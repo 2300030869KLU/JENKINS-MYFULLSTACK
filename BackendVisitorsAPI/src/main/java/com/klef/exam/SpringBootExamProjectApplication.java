ddc793fe1a0a8d2518564fc1a8c66124d3997bce
Nikhil <2300030869@kluniversity.in>
2025-09-08 10:06:23 +0530

    updated SpringBootApplication

--- BackendVisitorsAPI/src/main/java/com/klef/exam/SpringBootExamProjectApplication.java
+++ BackendVisitorsAPI/src/main/java/com/klef/exam/SpringBootExamProjectApplication.java
@@ -3,9 +3,10 @@ package com.klef.exam;
 import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootExamProjectApplication {
+public class SpringBootExamProjectApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExamProjectApplication.class, args);
